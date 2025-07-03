45b7e4c9d849ef49cc40fbd605d9850edfba5a8a
pc <pc@DESKTOP-VMM42G7>
2025-07-03 15:56:39 -0500

    feat(edit): :recycle: edit.env

--- BACKEND/domains/user_security/auth_service/app/register_user/model.js
+++ BACKEND/domains/user_security/auth_service/app/register_user/model.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../shared/config/db');
 
 const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
   email: {
     type: DataTypes.STRING,
     unique: true,
