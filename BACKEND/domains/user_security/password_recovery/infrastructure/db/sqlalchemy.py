bc288519611cd64e7204dc1144145df29e79966c
pc <pc@DESKTOP-VMM42G7>
2025-07-05 09:12:56 -0500

    feat(Edit Controllers and docker compose): :zap: Edit  Compose to up in aws

--- BACKEND/domains/user_security/password_recovery/infrastructure/db/sqlalchemy.py
+++ BACKEND/domains/user_security/password_recovery/infrastructure/db/sqlalchemy.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://"), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
